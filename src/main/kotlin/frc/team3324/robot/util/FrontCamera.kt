e6c6c8b65e77be22b13b5cba502ba2c8d3d490d3
CybisArk <everythingbagel03@gmail.com>
2021-02-27 15:54:16 -0500

    Created FrontCamera.kt

---
+++ src/main/kotlin/frc/team3324/robot/util/FrontCamera.kt
@@ -0,0 +1,12 @@
+package frc.team3324.robot.util
+
+import org.opencv.core.Core
+import org.opencv.core.CvType
+import org.opencv.core.Mat
+
+class FrontCamera {
+    init {
+        Camera.run()
+    }
+
+}
